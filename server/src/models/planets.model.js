3efc1e33735b0b2d14e385fb4fac0e29aef8c1a1
Samuel Andersson <58047562+samswede@users.noreply.github.com>
2023-12-18 23:53:34 +0100

    fixed on start data load

--- server/src/models/planets.model.js
+++ server/src/models/planets.model.js
@@ -1,5 +1,8 @@
-const parse = require('csv-parse');
 const fs = require('fs');
+const path = require('path');
+
+const { parse } = require('csv-parse');
+
 
 const habitablePlanets = [];
 
@@ -8,6 +11,23 @@ function isHabitablePlanet(planet) {
     && planet['koi_insol'] > 0.36 && planet['koi_insol'] < 1.11
     && planet['koi_prad'] < 1.6;
 }
+
+/*
+Below is the code we used to read the CSV file and parse it into a JavaScript object.
+We used the csv-parse package to parse the CSV file.
+We used the fs package to read the CSV file.
+
+We used the pipe method to pipe the data from the read stream to the parse stream.
+
+However, we are using promises and async/await to stream the data from the CSV file.
+
+This can result in the data being exported before the data is finished being streamed.
+
+We will turn this whole process into a promise so that we can use async/await to stream the data.
+
+Once again, below is how the code looked before we turned it into a promise.
+
+// ==============================
 
 fs.createReadStream('kepler_data.csv')
   .pipe(parse({
@@ -30,5 +50,41 @@ fs.createReadStream('kepler_data.csv')
   });
 
   module.exports = {
+    planets: habitablePlanets, // rename to planets
+    };
+
+// ==============================
+
+*/
+
+// note that when we import this, we will also need to use async/await:
+// const { loadPlanetsData } = require('./models/planets.model');
+// await loadPlanetsData();
+
+function loadPlanetsData() {
+    return new Promise((resolve, reject) => {
+        fs.createReadStream(path.join(__dirname, '..', '..', 'data', 'kepler_data.csv'))
+        .pipe(parse({
+            comment: '#',
+            columns: true,
+        }))
+        .on('data', (data) => {
+            if (isHabitablePlanet(data)) {
+            habitablePlanets.push(data);
+            }
+        })
+        .on('error', (err) => {
+            console.log(err);
+            reject(err);
+        })
+        .on('end', () => {
+            console.log(`${habitablePlanets.length} habitable planets found!`);
+            resolve();
+        });
+    });
+    }
+
+module.exports = {
+    loadPlanetsData,
     planets: habitablePlanets, // rename to planets
     };
--- server/src/server.js
+++ server/src/server.js
@@ -3,13 +3,28 @@ const http = require('http');
 
 const app = require('./app');
 
+const { loadPlanetsData } = require('./models/planets.model');
+
 const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
-server.listen(PORT, () => {
-    console.log(`Server listening on port ${PORT}`);
-});
+
+/*
+This below is a very common node pattern.
+We want to make sure that the data is loaded before we start the server.
+We will use async/await to do this.
+
+We could be loading a database, or a file, or anything else that takes time to load.
+*/
+async function startServer() {
+    await loadPlanetsData();
+    server.listen(PORT, () => {
+        console.log(`Server listening on port ${PORT}...`);
+    });
+}
+
+startServer();
 
 //const express = require('express');
 //const app = express();
